7c78df4a299c91e20de840732014d45d9d8200d4
Erik Muro <erikmuro@Eriks-iMac.local>
2015-11-29 20:16:03 -0600

    inializing repo and has both old modals and new lightbox modals

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+var portfolioApp = angular.module('portfolioApp', ['ngRoute', 'portfolioAppControllers', 'ui.bootstrap']);
+
+
+portfolioApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/portfolio', {
+        templateUrl: 'partials/portfolio.html',
+        controller: 'portfolioAppCtrl'
+      }).
+      when('/photography', {
+        templateUrl: 'partials/photography.html',
+        controller: 'portfolioAppCtrl'
+      }).
+      when('/resume', {
+        templateUrl: 'partials/resume.html',
+        controller: 'portfolioAppCtrl'
+      }).
+      when('/contact', {
+        templateUrl: 'partials/contact.html',
+        controller: 'portfolioAppCtrl'
+      }).
+      otherwise({
+        redirectTo: '/contact'
+      });
+  }]);
---
+++ public/js/controllers.js
@@ -0,0 +1,18 @@
+
+
+var portfolioAppControllers = angular.module('portfolioAppControllers', []);
+
+portfolioAppControllers.controller('portfolioAppCtrl', ['$scope', '$http' , function ($scope, $http) {
+
+    $scope.orderProp = 'erik';
+
+
+  }]);
+
+
+  $( document ).ready(function() {
+      lightbox.option({
+            'resizeDuration': 200,
+            'wrapAround': true
+          })
+  });
