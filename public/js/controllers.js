b178e6fe68032ab427db84527e30273a1725f2d0
Erik Muro <erikmuro@Eriks-iMac.local>
2015-12-07 18:16:27 -0600

    finshed views, need photos and resume to complete

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,15 +4,19 @@ var portfolioAppControllers = angular.module('portfolioAppControllers', []);
 
 portfolioAppControllers.controller('portfolioAppCtrl', ['$scope', '$http' , function ($scope, $http) {
 
-    $scope.orderProp = 'erik';
+
+    $scope.highlight = function(event) {
+        var id = event.target.id
+        $('.active-li').removeClass('active-li');
+        $("#"+id).addClass('active-li');
+    };
+
+}]);
 
 
-  }]);
-
-
-  $( document ).ready(function() {
-      lightbox.option({
-            'resizeDuration': 200,
-            'wrapAround': true
-          })
-  });
+$( document ).ready(function() {
+  lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+      })
+});
